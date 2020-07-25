ba7f955a51c2935b4ff559da9c4e4e4e0f70adb0
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-07-25 09:18:20 -0300

    Implementação da classe Account e da Classe DomainExceptions para usar na consistência do programa. inclusão de regra para validação de saque. Uso do try-catch.

---
+++ SaqueContaBancariaComExcecao/Entities/Account.cs
@@ -0,0 +1,45 @@
+﻿using SaqueContaBancariaComExcecao.Entities.Exceptions;
+
+namespace SaqueContaBancariaComExcecao.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if(amount > WithdrawLimit)
+            {
+                throw new DomainExceptions("Withdraw error: The amount exceeds withdraw limit.");
+            }
+            else if (amount > Balance)
+            {
+                throw new DomainExceptions("Withdraw error: Not enough balance.");
+            }
+            else
+            {
+                Balance -= amount;
+            }            
+        }
+
+        public void Deposit(double amout)
+        {
+            Balance += amout;
+        }
+
+    }
+}
--- SaqueContaBancariaComExcecao/Program.cs
+++ SaqueContaBancariaComExcecao/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using SaqueContaBancariaComExcecao.Entities;
+using System.Globalization;
 
 namespace SaqueContaBancariaComExcecao
 {
@@ -6,7 +8,39 @@ namespace SaqueContaBancariaComExcecao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int nr_account = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string nm_holder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double nr_iniBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw Limit: ");
+                double nr_WithdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(nr_account, nm_holder, nr_iniBalance, nr_WithdrawLimit);
+
+                Console.Write("Enter amount for deposit: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                acc.Deposit(amount);
+
+                Console.Write("Enter amount for withdraw: ");
+                amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                acc.Withdraw(amount);                
+
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }            
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }            
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
